f160b3630423f207d048a38c8e5330ad5ef1600d
Steven Burris <stevenburris.jr@gmail.com>
2016-11-17 15:42:38 -0500

    Broke everything so I can do math for loan logic!!

--- src/main/java/com/youngburris/controllers/StudentStocksRestController.java
+++ src/main/java/com/youngburris/controllers/StudentStocksRestController.java
@@ -31,6 +31,9 @@ import java.util.ArrayList;
 @RestController
 public class StudentStocksRestController {
 
+
+
+
     @Autowired
     StudentRepository students;
 
@@ -195,17 +198,6 @@ public class StudentStocksRestController {
         return new ResponseEntity<ArrayList<Portion>>(portionArrayList, HttpStatus.OK);
     }
 
-    public void loanPaymentCalculator(int gracePeriod, double presentValue, double apr, double years) {
-//        get the periodic interest rate from the annual percentage rate
-        double decimal = apr / 100.00;
-        double r = apr / 12;
 
-//        get the periods in the loan from number of years
-        double n = years * 12;
-
-//        calculate the payment
-        double payment = (r * presentValue) / (1 - Math.pow((1 + r), n));
-
-    }
 
 }
