37da66d441b1f3945713d4309d4db6b61ab4e868
Aaron LeCheminant <Aaron@Brelyns-MacBook-Pro-2.local>
2016-11-19 16:19:36 -0500

    update form

--- src-client/scripts/student-signup.js
+++ src-client/scripts/student-signup.js
@@ -1,4 +1,5 @@
 const React = require('react')
+const ACTIONS = require('./actions.js')
 
 
 
@@ -6,6 +7,32 @@ const React = require('react')
 
 
 const StudentFormModal = React.createClass ({
+
+
+   _handleClick: function(evt){
+      evt.preventDefault()
+
+      let newForm ={
+         email: this.refs.email.value,
+         password: this.refs.password.value,
+         gpa: this.refs.gpa.value,
+         firstName: this.refs.firstName.value,
+         highSchool: this.refs.highSchool.value,
+         lastName: this.refs.last.value,
+         level: this.refs.level.value,
+         loanGoal: this.refs.loanGoal.value,
+         major: this.refs.major.value,
+         minor: this.refs.minor.value,
+         school: this.refs.school.value,
+         ssn: this.refs.ssn.value,
+         bio: this.refs.bio.value
+      }
+      console.log(newForm)
+      ACTIONS.submitStudentForm(newForm)
+
+
+
+   },
 
 
    render: function() {
@@ -16,44 +43,76 @@ const StudentFormModal = React.createClass ({
          <div className="form-cont">
             <form action="" className="form-horizontal">
                {/* username */}
-               <SimpleInput fieldType="text" nameTag="Email" ref="email"/>
+               <div className="input-group simp-input">
+                  <label htmlFor="email" className="input-label">Email</label>
+                  <input type="text" className="form-control" placeholder="Email" ref="email"/>
+               </div>
                {/* password */}
-               <SimpleInput fieldType="password" nameTag="Password" ref="password"/>
+               <div className="input-group simp-input">
+                  <label htmlFor="password" className="input-label">Password</label>
+                  <input type="password" className="form-control" placeholder="Password" ref="password"/>
+               </div>
                {/* firstName */}
-               <SimpleInput fieldType="text" nameTag="First Name" ref="first"/>
+               <div className="input-group simp-input">
+                  <label htmlFor="first" className="input-label">Fist Name</label>
+                  <input type="text" className="form-control" placeholder="First Name" ref="firstName"/>
+               </div>
                {/* lastName */}
-               <SimpleInput fieldType="text" nameTag="Last Name" ref="last"/>
-               {/* bio */}
+               <div className="input-group simp-input">
+                  <label htmlFor="last" className="input-label">Last Name</label>
+                  <input type="text" className="form-control" placeholder="Last Name" ref="last"/>
+               </div>
 
                {/* school */}
-               <SimpleInput fieldType="text" nameTag="School Attending" ref="school"/>
+               <div className="input-group simp-input">
+                  <label htmlFor="school" className="input-label">School Attending</label>
+                  <input type="text" className="form-control" placeholder="School Attending" ref="school"/>
+               </div>
                {/* bio */}
                <label htmlFor="bio">Tell us a little about yourself...</label>
-               <textarea className="form-control" rows="3" id="textArea" ></textarea>
+               <textarea className="form-control" rows="3" id="textArea" ref="bio"></textarea>
                {/* highSchool */}
-               <SimpleInput fieldType="text" nameTag="High School" ref="highSchool"/>
-               {/* transcript */}
-               <SimpleInput fieldType="text" nameTag="Transcript" ref="transcipt"/>
+               <div className="input-group simp-input">
+                  <label htmlFor="highSchool" className="input-label">High School</label>
+                  <input type="text" className="form-control" placeholder="High School" ref="highSchool"/>
+               </div>
+               
                {/* level */}
                Graduate or Undergrad?
                <select className="form-control" id="select" ref="level">
-                  <option>Undergraduate</option>
-                  <option>Graduate</option>
+                  <option>UNDERGRADUATE</option>
+                  <option>GRADUATE</option>
                </select>
-
                {/* gpa */}
-               <SimpleInput fieldType="text" nameTag="G.P.A" ref="gpa"/>
+               <div className="input-group simp-input">
+                  <label htmlFor="gpa" className="input-label">G.P.A</label>
+                  <input type="text" className="form-control" placeholder="G.P.A" ref="gpa"/>
+               </div>
                {/* major */}
-               <SimpleInput fieldType="text" nameTag="Major" ref="major"/>
+               <div className="input-group simp-input">
+                  <label htmlFor="major" className="input-label">Major</label>
+                  <input type="text" className="form-control" placeholder="Major" ref="major"/>
+               </div>
                {/* minor */}
-               <SimpleInput fieldType="text" nameTag="Minor" ref="minor"/>
+               <div className="input-group simp-input">
+                  <label htmlFor="password" className="input-label">Minor</label>
+                  <input type="text" className="form-control" placeholder="Minor" ref="minor"/>
+               </div>
                {/* ssn */}
-               <SimpleInput fieldType="text" nameTag="SSN" ref="ssn"/>
+               <div className="input-group simp-input">
+                  <label htmlFor="ssn" className="input-label">SSN</label>
+                  <input type="text" className="form-control" placeholder="SSN" ref="ssn"/>
+               </div>
                {/* loanGoal */}
-               <SimpleInput fieldType="text" nameTag="Loan Amount" ref="loanGoal"/>
+               <div className="input-group simp-input">
+                  <label htmlFor="loanGoal" className="input-label">Loan Amount</label>
+                  <input type="text" className="form-control" placeholder="Loan Amount" ref="loanGoal"/>
+               </div>
+
+               <button className="btn button-primary" onClick={this._handleClick}>Submit Form</button>
             </form>
 
-            </div>
+         </div>
 
 
                )
@@ -63,22 +122,6 @@ const StudentFormModal = React.createClass ({
 
 
                })
-const SimpleInput = React.createClass({
 
-
-
-
-
-   render: function(){
-
-      return (
-
-         <div className="input-group simp-input">
-            <label htmlFor={this.props.nameTag} className="input-label">{this.props.nameTag}</label>
-            <input type={this.props.fieldType} className="form-control" placeholder={this.props.nameTag}/>
-         </div>
-      )
-   }
-})
 
 module.exports = {StudentFormModal}
